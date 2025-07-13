fef18e5891074abaa0355cd069c816fb1caaee06
Ashu2607 <ashutosh.snh@iitg.ac.in>
2025-07-13 12:03:37 +0530

    Update signUppage.jsx

--- src/components/signUppage.jsx
+++ src/components/signUppage.jsx
@@ -34,6 +34,7 @@ function SignUpPage() {
 
   
     try {
+      
       const response = await fetch("http://localhost:5000/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
